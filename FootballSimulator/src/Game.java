c30a6a2690ef8cb40209087caf3d3ff7e8b2eb60
Kshitiz Thapa <kthapa@uwaterloo.ca>
2017-07-22 14:03:42 -0400

    Update the game, now it handles neutral venues as well. In terms of bug fixes, I have fixed the earlier system because it wasnt displaying the right game predictions. This has been fixed, you can find the comments in the code

--- FootballSimulator/src/Game.java
+++ FootballSimulator/src/Game.java
@@ -4,44 +4,107 @@ public class Game {
 	
 	int rank_team1;
 	int rank_team2;
+	int diff;
+	int probability;
 	
 	public Game(int r1, int r2){
 		rank_team1=r1;
 		rank_team2=r2;
+		if(rank_team1 < rank_team2){
+			diff= rank_team2- rank_team1;
+		}else{
+			diff= rank_team1- rank_team2;
+		}
+		
+		if(diff<= 10){
+			probability=50;
+		}else if ((diff>10)&&(diff <=20) ){
+			probability=45;
+		}else if((diff > 20)&&(diff <= 30)){
+			probability=40;
+		}else if((diff > 30)&&(diff <= 40)){
+			probability=35;
+		}else if((diff > 40)&&(diff <= 50)){
+			probability=30;
+		}else if((diff > 50)&&(diff <= 60)){
+			probability=25;
+		}else if((diff > 60)&&(diff <= 70)){
+			probability=20;
+		}else if((diff > 70)&&(diff <= 80)){
+			probability=15;
+		}else if((diff > 80)){
+			probability=10;
+		}
+		
 	}
 	
-	public void whoWouldWin(){
-		System.out.println("I am assuming that team1 is the team that is playing at home");
+	public int whoWouldWinHomeAway(){		
+		
 		Random rand= new Random();
 		//Playing at home gives the home team a 5% increase chance in winning
+		int whoWillWin= rand.nextInt(100);
+
+		
 		if(rank_team1 < rank_team2){
-			int whoWillWin= rand.nextInt(100);
-			System.out.println(whoWillWin);
+			//team1 is home team and the better team
 			
-			if(whoWillWin > 45){
-				System.out.println("No shock here! Team 1 has won ");
-				//return 1;
+			//I take probability-5 because assuming difference is between 30 and 40.
+			//probability will be 35, but since team1 has an advantage that number will go down to 30
+			//Thus, the variable whoWillWIn has 70 chances to choose team1, and 30 chances for team2. 
+			
+			if(whoWillWin > (probability-5)){
+			//	System.out.println("No shock here! Team 1 has won");
+				return 1;
 			}else{
-				System.out.println("In a shock here, team 2 has won!!");
-				//return 2;
+				//System.out.println("In a shock here, team 2 has won!!");
+				return 2;
 			}
 			
 		}else{
-			//Since team 1 is a lower team than team2, but team1 is playing at home. 
-			//So their probability will go from 55% to 50%
 			
-			int whoWillWin= rand.nextInt(100);
-			if(whoWillWin > 50){
-				System.out.println(" Team 2 has won ");
-				//return 2;
+			//Team2 is away team and the better team., i.e rank_team2<rank_team1
+			
+			
+			//I take probability+5 because assuming difference is between 30 and 40.
+			//probability will be 35, but since team1 has an advantage that number will go up to 40
+			//Thus, the variable whoWillWIn has 40 chances to choose team1, and 60 chances for team2.
+			
+			if(whoWillWin < (probability+5)){
+		//		System.out.println("Home advantage does count, and Team 1 has won");
+				return 1;
 			}else{
-				System.out.println(" team 1 has won!!");
-			//	return 1;
+			//	System.out.println("Well good teams find a way to win and team 2 has won!!");
+				return 2;
 
 			}
 	
 		}
-		//System.out.println("end");
+	}
+	
+	public int whoWouldWinNeutral(){
+		Random rand= new Random();
+		if(rank_team1 < rank_team2){
+			int whoWillWin= rand.nextInt(100);
+			//System.out.println("ream1<team2");
+			if(whoWillWin > (probability)){
+			//	System.out.println("Well team 2 has clinched the victory, somehow! ");
+				return 1;
+			}else{
+			//	System.out.println("No surpise here, team 1 has won!");
+				return 2;
+			}
+			
+		}else{
+			int whoWillWin= rand.nextInt(100);
+			if(whoWillWin < (probability)){
+			//	System.out.println("In a shock here, team 1 has won!!");
+
+				return 1;
+			}else{
+			//	System.out.println("No shock here! Team 2 has won ");
+				return 2;
+			}
+		}
 	}
 	
 	
--- FootballSimulator/src/main.java
+++ FootballSimulator/src/main.java
@@ -3,7 +3,41 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Game gm= new Game(2,20);
-		gm.whoWouldWin();
+		Game gm= new Game(41,1);
+		int team1=0;
+		int team2=0;
+		int i=0;
+		System.out.println(gm.diff);
+		System.out.println(gm.probability );
+		
+		while (i<1000){
+		
+		if(gm.whoWouldWinHomeAway() == 1){
+			team1++;
+		}else{
+			team2++;
+		}
+		i++;
+		}
+		System.out.println("Home and away");
+		System.out.println("team1: "+ team1);
+		System.out.println("team2: "+ team2);
+
+		
+		i=0;
+		team1=0;
+		team2=0;
+		while (i<1000){
+			if(gm.whoWouldWinNeutral() == 1){
+				team1++;
+			}else{
+				team2++;
+			}
+			i++;
+		}
+		System.out.println("Neutral");
+		System.out.println("team1: "+ team1);
+		System.out.println("team2: "+ team2);
+		
 	}
 }
